6d3a784563d8ce5e2b459d1a2bb4196fa5166b35
djl <yuuyoo@163.com>
2019-06-28 19:36:39 +0800

    微信支付功能实现

---
+++ shop_payApi/src/main/java/com/djl/shop/pay/PayApplication.java
@@ -0,0 +1,25 @@
+package com.djl.shop.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author DJL
+ * @create 2019-06-27 22:39
+ * @desc ${DESCRIPTION}
+ **/
+@SpringBootApplication
+@EnableSwagger2
+public class PayApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PayApplication.class);
+    }
+}
